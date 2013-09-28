1efa1729a318dc3bfa8725263e7c01cd8f9cfc6e
Markus Holtermann <info@markusholtermann.eu>
2013-09-28 14:09:21 +0200

    Load the content of the cookiecutter.json file if it exists
    If no cookiecutter.json file exists, fall back to another json file if
    there is exactly one in the root level of a repo. If multiple json files
    exist there, fail as we don't know which file to use.

---
+++ bakery/cookies/exceptions.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+class InvalidRepositoryError(ValueError):
+    pass
+
+
+class InvalidContentFileEncoding(ValueError):
+    pass
--- bakery/cookies/managers.py
+++ bakery/cookies/managers.py
@@ -2,6 +2,7 @@
 
 from django.db.models import Manager
 
+from bakery.auth.models import BakeryUser
 from bakery.utils.vcs import gh
 
 
@@ -12,9 +13,19 @@ class CookieManager(Manager):
         if 'git@github.com' in url or 'https://github.com/' in url:
             repo = gh.get_repo_from_url(url)
             cookie_data = gh.get_cookie_data_from_repo(repo)
+            owner_data = cookie_data.pop('_owner', None)
         else:
             raise ValueError('{0} is not a recognized URL')
 
-        cookie = self.model(**cookie_data)
+        return self.insert_from_cookie_dict(cookie_data, owner_data)
+
+    def insert_from_cookie_dict(self, cookie_dict, owner_dict):
+        owner, created = BakeryUser.objects.get_or_create(**owner_dict)
+        if created:
+            owner.set_unusable_password()
+            owner.save(update_fields=['password'])
+        cookie_dict['owner'] = owner
+
+        cookie = self.model(**cookie_dict)
         cookie.save()
         return cookie

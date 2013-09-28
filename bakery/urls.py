966f6864cadb9884eb716458fc2c22853de4f7fc
Christopher Grebs <cg@webshox.org>
2013-09-28 14:09:21 -0700

    LogoutView

--- bakery/urls.py
+++ bakery/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import patterns, url, include
 
 
 urlpatterns = patterns('',
-    url(r'^$', 'bakery.views.index', name='index'),
+    url(r'^$', 'bakery.views.home', name='home'),
+    url(r'^logout/$', 'bakery.views.logout', name='logout'),
     url(r'^login-error/$', 'bakery.views.login_error', name='login_error'),
     url('', include('social.apps.django_app.urls', namespace='social'))
 )
--- bakery/views.py
+++ bakery/views.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView, RedirectView
+from django.contrib import auth
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
-index = IndexView.as_view()
+home = HomeView.as_view()
 
 
 class LoginErrorView(TemplateView):
     template_name = 'error.html'
 
 login_error = LoginErrorView.as_view()
+
+
+class LogoutView(RedirectView):
+    permanent = False
+
+    def get_redirect_url(self, **kwargs):
+        auth.logout(self.request)
+        return reverse('home')
+
+logout = LogoutView.as_view()

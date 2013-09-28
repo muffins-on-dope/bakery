61d43750bc75c3f480992c8feaae392fc8621594
Christopher Grebs <cg@webshox.org>
2013-09-28 13:34:42 -0700

    Fix tests, cleanups

--- bakery/views.py
+++ bakery/views.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import FormView, TemplateView
-from django.shortcuts import render
-from django.contrib import messages
-from django.contrib import auth
-from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):

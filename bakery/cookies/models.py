c0984821e2c9a1e54efc953a4b0d703f0e310fcc
Markus Holtermann <info@markusholtermann.eu>
2013-09-28 03:02:28 +0200

    Add basic cookie model
    [skip ci]

--- bakery/cookies/models.py
+++ bakery/cookies/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from jsonfield import JSONField
+
+
+class Cookie(models.Model):
+    name = models.CharField(_('Name'), max_length=50)
+    url = models.URLField(_('URL'))
+    owner = models.ForeignKey(get_user_model(), verbose_name=_('User'),
+        on_delete=models.CASCADE)
+    description = models.TextField(_('Description'), default='')
+    license = models.CharField(_('License'), max_length=50, default='')
+    last_change = models.DateTimeField(_('Last change'))
+    last_post = models.DateTimeField(_('Last poll'))
+    mapping = JSONField()
+
+    class Meta:
+        verbose_name = _('Cookie')
+        verbose_name_plural = _('Cookies')
+
+    def __unicode__(self):
+        return self.name

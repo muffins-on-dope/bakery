fd674f16f2105f16ae8b4ace9e608d4b81548505
Christopher Grebs <cg@webshox.org>
2013-09-27 20:33:35 -0700

    fix typo

--- bakery/urls.py
+++ bakery/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'bakery.views.index', name='index'),
-    url(r'^style_demo$', 'bakery.views.style_demo', name='style_demo'),
+    url(r'^style_demo/$', 'bakery.views.style_demo', name='style_demo'),
 )

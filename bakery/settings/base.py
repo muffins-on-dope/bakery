25d60cc148fc3d2e04bd969f3072a9479d432d21
Christopher Grebs <cg@webshox.org>
2013-09-27 20:45:12 -0700

    Add social apps installed app

--- bakery/settings/base.py
+++ bakery/settings/base.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
+    'social.apps.django_app.default',
     'bakery',
     'bakery.cookies',
 )

8d498afc08d01a801044713ae430d85dfef7d8b6
Markus Holtermann <info@markusholtermann.eu>
2013-09-29 17:01:43 +0200

    Fix url mapping

--- bakery/cookies/urls.py
+++ bakery/cookies/urls.py
@@ -3,5 +3,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('bakery.cookies.views',
-    url(r'^cookie/(?P<owner_name>[^/]+)/(?P<name>[^/]+)$', 'detail', name='detail'),
+    url(r'^cookie/(?P<owner_name>[^/]+)/(?P<name>[^/]+)/$', 'detail', name='detail'),
 )

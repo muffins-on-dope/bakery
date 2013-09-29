7c098ffa821ded1a6404b1453fe4587cb7500dbf
Christopher Grebs <cg@webshox.org>
2013-09-29 11:20:30 -0700

    fix auth.urls

--- bakery/auth/urls.py
+++ bakery/auth/urls.py
@@ -2,9 +2,8 @@
 from django.conf.urls import patterns, url, include
 
 
-urlpatterns = patterns('',
-    url(r'^logout/$', 'bakery.views.logout', name='logout'),
-    url(r'^login-error/$', 'bakery.views.login_error', name='login_error'),
-
-    url(r'^profile/(?P<username>.*?)/$', 'bakery.views.profile', name='profile'),
+urlpatterns = patterns('bakery.auth.views',
+    url(r'^logout/$', 'logout', name='logout'),
+    url(r'^login-error/$', 'login_error', name='login_error'),
+    url(r'^profile/(?P<username>.*?)/$', 'profile', name='profile'),
 )

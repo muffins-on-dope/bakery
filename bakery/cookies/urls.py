91a12e0bd25924f4c980be8856fce77390215d17
Markus Holtermann <info@markusholtermann.eu>
2013-09-29 11:49:16 +0200

    Trivial cookie detail view

---
+++ bakery/cookies/urls.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('bakery.cookies.views',
+    url(r'^cookie/(?P<owner_name>[^/]+)/(?P<name>[^/]+)$', 'detail', name='detail'),
+)
--- bakery/cookies/views.py
+++ bakery/cookies/views.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from django.http import Http404
+
+
+def detail(request, owner_name, name):
+    raise Http404('{0}/{1}'.format(owner_name, name))

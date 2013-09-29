395da2fb12404aaf625ac313ebc8658229c56591
Christopher Grebs <cg@webshox.org>
2013-09-29 10:15:59 -0700

    fixed admin

--- bakery/profiles/admin.py
+++ bakery/profiles/admin.py
@@ -3,19 +3,19 @@ from django.contrib import admin
 from bakery.profiles.models import Profile
 
 
-class ProfileAdmin(VersionAdmin):
+class ProfileAdmin(admin.ModelAdmin):
 
-    search_fields = ("user__username", "github_account", "user__email", "email")
-    list_display = ("github_account", "email", 'username_display', 'username_display')
+    search_fields = ("user__username", "user__email")
+    list_display = ('username_display', 'user_email_display')
 
     def username_display(self, obj):
         return (obj.user.username)
-    username.short_description = "User username"
+    username_display.short_description = "User username"
 
 
     def user_email_display(self, obj):
         return (obj.user.email)
-    username.short_description = "User email"
+    username_display.short_description = "User email"
 
 
 admin.site.register(Profile, ProfileAdmin)

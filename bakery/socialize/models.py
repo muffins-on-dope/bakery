11457f9e5765bb4b3d9ba6c48bad4d364ed4a88b
Markus Holtermann <info@markusholtermann.eu>
2013-09-29 10:44:00 +0200

    Integrate actually use VoteManager

--- bakery/socialize/models.py
+++ bakery/socialize/models.py
@@ -2,11 +2,19 @@
 
 from django.db import models
 
-from bakery.auth.models import BakeryUser
-from bakery.cookies.models import Cookie
+from bakery.socialize.managers import VoteManager
 
 
 class Vote(models.Model):
-    user = models.ForeignKey(BakeryUser)
-    cookie = models.ForeignKey(Cookie)
+    cookie = models.ForeignKey('cookies.Cookie')
+    user = models.ForeignKey('auth.BakeryUser')
+
     datetime = models.DateTimeField(auto_now_add=True)
+
+    objects = VoteManager()
+
+    class Meta:
+        unique_together = ('user', 'cookie')
+
+    def __repr__(self):
+        return '<Vote: cookie: {0}, user: {1}>'.format(self.cookie, self.user)
